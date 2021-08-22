4b145faefaacb0f0bd11beb1b9351f73c56869b4
savanna chiang <savannaac@gmail.com>
2021-08-22 00:50:29 -0500

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // are we missing an import?
+import Comment from './Comment';
 
 export default class BlogPost extends Component {
   render() {
@@ -23,6 +24,9 @@ export default class BlogPost extends Component {
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[0]} />
         */}
+        <Comment commentText={comments[0]} />
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
       </div>
     );
   }
